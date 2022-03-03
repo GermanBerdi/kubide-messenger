3bbaa2b23e07370a6af532cfae328e08e54bcd49
German Berdichevsky <german.metha@gmail.com>
2022-03-03 18:51:46 +0100

    He creado health-controller como practica para ver como crear distintos endpoints

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+/*TODO:
+  He creado health-controller como practica para ver como crear distintos endpoints. Devuelve un string, hay que cambiarlo por un JSON
+
+
+*/
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
